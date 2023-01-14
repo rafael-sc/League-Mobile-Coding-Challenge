59223d5d4b9532373eac2f455da23e80b7528f6e
Rafael <rafael.costa.ti@gmail.com>
2023-01-14 19:42:10 -0300

    adding PostsUseCaseTest.kt

---
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/PostsUseCaseTest.kt
@@ -0,0 +1,84 @@
+package life.league.challenge.kotlin
+
+import io.kotlintest.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import life.league.challenge.kotlin.commom.exceptions.UnableToLoginException
+import life.league.challenge.kotlin.domain.model.Post
+import life.league.challenge.kotlin.domain.model.User
+import life.league.challenge.kotlin.domain.repository.LoginRepository
+import life.league.challenge.kotlin.domain.repository.PostsRepository
+import life.league.challenge.kotlin.domain.usecase.posts.PostsUseCase
+import life.league.challenge.kotlin.domain.usecase.posts.PostsUseCaseImpl
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class PostsUseCaseTest {
+
+    private lateinit var postsUseCase: PostsUseCase
+
+    @MockK
+    private lateinit var postsRepository: PostsRepository
+
+    @MockK
+    private lateinit var loginRepository: LoginRepository
+
+    private val mockedPostsList = listOf(
+        Post(
+            id = 0,
+            title = "title",
+            content = "content",
+            user = User(
+                id = 0,
+                name = "User 0",
+                avatarUrl = "http://avatar.url/0.jpg"
+            )
+        ),
+        Post(
+            id = 1,
+            title = "title",
+            content = "content",
+            user = User(
+                id = 1,
+                name = "User 1",
+                avatarUrl = "http://avatar.url/1.jpg"
+            )
+        )
+    )
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        postsUseCase = PostsUseCaseImpl(loginRepository, postsRepository)
+    }
+
+    @Test
+    fun `getPosts() - user is authenticated - should return mocked Posts`() = runTest {
+        // arrange
+        coEvery { loginRepository.isUserAuthenticated() } returns true
+        coEvery { postsRepository.getPosts() } returns mockedPostsList
+
+        // act
+        val receivedPosts = postsUseCase.getPosts()
+
+        // assert
+        receivedPosts shouldBe mockedPostsList
+    }
+
+    @Test(expected = UnableToLoginException::class)
+    fun `getPosts() - user is not authenticated - should return mocked Posts`() = runTest {
+        // arrange
+        coEvery { loginRepository.isUserAuthenticated() } returns false
+        coEvery { loginRepository.login(any(), any()) } throws UnableToLoginException()
+
+        // act
+        val receivedPosts = postsUseCase.getPosts()
+
+        // assert
+        receivedPosts shouldBe mockedPostsList
+    }
+}
